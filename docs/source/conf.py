56a9b6e829319d3505f0bdad1d2861fe2245c969
Elvis <elvis.degeitere1@gmail.com>
2025-10-24 19:59:16 +0200

    Lint conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,7 @@ import os
 import sys
 import django
 
-sys.path.insert(0, os.path.abspath('../..'))  
+sys.path.insert(0, os.path.abspath('../..'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oc_lettings_site.settings')
 django.setup()
 
@@ -14,11 +14,11 @@ release = '1.0.0'
 
 # -- Extensions Sphinx -----------------------------------------------------------
 extensions = [
-    'sphinx.ext.autodoc',       
-    'sphinx.ext.napoleon',      
-    'sphinx.ext.viewcode',      
-    'sphinx.ext.todo',          
-    'myst_parser',              
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+    'myst_parser',
 ]
 
 templates_path = ['_templates']
